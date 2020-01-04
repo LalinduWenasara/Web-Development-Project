4d5c9bbfb62577a3b97bfc5ef7c2d4192f90efc8
Lalindu Wenasara <lalinduwenu@gmail.com>
2020-01-04 09:53:24 +0530

    added menu page item adding page and sql db
    working timeline reference
    
    https://github.com/LalinduWenasara/Web_App_Development_Project/network

---
+++ js/myScript1.js
@@ -0,0 +1,43 @@
+/*
+function passwordlog() {
+
+var password;
+var password2=plymouth19;
+password=prompt('enter your admin password');
+if(password2==password)
+window.location="uplord.php";
+else{
+    window.location="index.html";
+}
+*/
+    
+}
+
+/*window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}*/
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
